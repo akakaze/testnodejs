8c4cfab8a59ddb36d5809f6e5b95524f6a42686a
akakaze <m2251000@gmail.com>
2016-08-09 01:56:19 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,7 @@
 /**
  * Created by PC on 2016/8/8.
  */
+ var request = require('request'),
+    customRequest = request.defaults({'proxy': process.env.FIXIE_URL});
+customRequest.get("http://www.example.com");
 console.log(process.env);
