19043f0cafe0353ade2d374b0bf0949b563bb2ba
akakaze <m2251000@gmail.com>
2016-08-09 18:20:35 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,18 @@
 /**
  * Created by PC on 2016/8/8.
  */
-var request = require('request'),
-    customRequest = request.defaults({'proxy': process.env.FIXIE_URL});
-customRequest.get("http://www.example.com");
-console.log(process.env);
+var http = require('http'),
+    url = require("url"),
+    fixieUrl = url.parse(process.env.FIXIE_URL),
+    requestUrl = url.parse("http://www.example.com");
+http.get({
+    host: fixieUrl.hostname,
+    port: fixieUrl.port,
+    path: requestUrl.href,
+    headers: {
+      Host: requestUrl.host,
+      "Proxy-Authorization": "Basic " + new Buffer(fixieUrl.auth).toString('base64'),
+    }
+}, function (res) {
+  console.log("Got response: " + res.statusCode);
+});
